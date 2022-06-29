3786f165a99010bbaf3d4a1302caa5d3cc980683
rotex <dantejake48@gmail.com>
2022-06-29 16:49:41 +0100

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+*Description: print "Programming is like building a multilingual puzzle"
+*
+* Return: returns value 0
+*/
+int main(void)
+{
+char str[52] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
+
