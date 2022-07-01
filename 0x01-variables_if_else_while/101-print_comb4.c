86b4215fb632ff2314ff91c2649ec0556dbfa9ac
rotex <dantejake48@gmail.com>
2022-07-01 23:06:09 +0100

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all possible different combinations of 3 digits.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+        
+        i = 0;
+	while (i < 1000)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+                i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
