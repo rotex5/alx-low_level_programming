555232d201cc4428a6b33372b2e77af4c2242fd3
rotex <dantejake48@gmail.com>
2022-07-05 12:13:58 +0100

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*calling the main.h file which contain function prototype **/
+
+
+/**
+ * main - Entry point
+ *
+ * Description : program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
