f29dd097a385df8c49f3842ebdcc3f45364d1ad0
rotex <dantejake48@gmail.com>
2022-07-05 13:06:47 +0100

    adjustments based on betty styling

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - prints alphabets in small letters
  *
- * Description : a function that prints the alphabet, in lowercase
- *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
-{      
+{
 
-        char alpha = 'a';
-        
+char alpha = 'a';
 
-        while(alpha <= 'z')
-        {       
-                _putchar(alpha);
-                alpha++;
 
-        }
-        _putchar('\n');
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+
+}
+_putchar('\n');
 }
