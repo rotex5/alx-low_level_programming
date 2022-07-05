29c553cba03b1f975c802e925a34a98d4499f891
rotex <dantejake48@gmail.com>
2022-07-05 20:53:10 +0100

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int sum = 0;
+
+i = 0;
+while (i < 1024)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+{
+sum += i;
+}
+i++;
+}
+printf("%d\n", sum);
+return (0);
+}
