07a385c4789b7d82bdc687d475dbda15b8a30828
rotex <dantejake48@gmail.com>
2022-07-07 15:02:42 +0100

    task 9 correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,23 +10,26 @@ int main(void)
 int i;
 
 i = 1;
+printf("%d", i);
+
+i = 2;
 while (i <= 100)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf(" Fizz");
 }
 else if ((i % 5 == 0))
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else
 {
-printf("%d ", i);
+printf(" %d", i);
 }
 
 i++;
