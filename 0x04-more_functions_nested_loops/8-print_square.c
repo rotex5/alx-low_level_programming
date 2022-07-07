7794d7cc9953a6773b2c9859d817e57c1afd702d
rotex <dantejake48@gmail.com>
2022-07-07 14:08:18 +0100

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ */
+
+void print_square(int size)
+{
+int a;
+int i;
+
+i = 0;
+while (i < size)
+{
+a = 0;
+while (a < size)
+{
+_putchar(35);
+a++;
+}
+_putchar('\n');
+i++;
+}
+/**_putchar('\n');**/
+}
