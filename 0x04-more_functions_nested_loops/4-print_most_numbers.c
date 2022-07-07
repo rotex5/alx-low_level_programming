e4202fde348937a7064e26354059b48d6efa75f0
rotex <dantejake48@gmail.com>
2022-07-07 11:45:25 +0100

    task 4 correction betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
+
 void print_most_numbers(void)
 {
-        int num;
+int num;
 
-        num = 48;
-        while (num <=57)
-        {
-                if (num == 50 || num == 52)
-                {       
-                        num++;
-                        continue;
-                }
-                else
-                {
-                        _putchar(num);
-                }
-                num++;
-        }
-        _putchar('\n');
+num = 48;
+while (num <= 57)
+{
+if (num == 50 || num == 52)
+{
+num++;
+continue;
+}
+else
+{
+_putchar(num);
+}
+num++;
+}
+_putchar('\n');
 }
