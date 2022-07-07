aad7c0b2740a808d909cbad2edb09cbba18769c3
rotex <dantejake48@gmail.com>
2022-07-07 11:03:50 +0100

    adjustment based on betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+
 int _isupper(int c)
 {
-        int alpha;
+int alpha;
 
-        alpha = 65;
-        while (alpha <= 90)
-        {
-                if (c == alpha)
-                {
-                        return (1);
-                }
-                else
-                {
-                        return (0);
-                }
-        }
-        return (0);
+alpha = 65;
+while (alpha <= 90)
+{
+if (c == alpha)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+return (0);
 }
