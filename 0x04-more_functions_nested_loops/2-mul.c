9c459c124a8c2691e81e59fc0a6c2d69aeff62d5
rotex <dantejake48@gmail.com>
2022-07-07 11:18:58 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: result of the multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+int mul_result;
+mul_result = a * b;
+return (mul_result);
+}
