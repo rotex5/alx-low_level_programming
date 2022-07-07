edea7937499e0a0929fbb4c6199208c34df14f75
rotex <dantejake48@gmail.com>
2022-07-07 13:01:20 +0100

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (n < 0)
+{
+_putchar('\n');
+}
+else
+{
+i = 0;
+while (i < n)
+{
+_putchar('_');
+i++;
+}
+_putchar('\n');
+}
+}
