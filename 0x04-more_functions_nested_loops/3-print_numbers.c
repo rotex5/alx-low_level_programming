db6fa9a6e9e954f3f7abb96d14073e526e97be47
rotex <dantejake48@gmail.com>
2022-07-07 11:29:30 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+
+void print_numbers(void)
+{
+int num;
+num = 48;
+while (num <= 57)
+{
+_putchar(num);
+num++;
+}
+_putchar('\n');
+}
