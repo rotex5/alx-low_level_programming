99647727083f4df45bf5b8e8746dfba5f1b92b7b
rotex <dantejake48@gmail.com>
2022-07-07 14:53:38 +0100

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i;
+
+i = 1;
+while (i <= 100)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if ((i % 5 == 0))
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+
+i++;
+}
+printf("\n");
+
+
+return (0);
+}
