c227ffc29e6f7592c27bef977c1566b57115114d
rotex <dantejake48@gmail.com>
2022-07-12 20:32:33 +0100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of a string
+* @str: pointer to the string to print
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+	int len;
+	int counter;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	counter = len / 2;
+	while (str[counter])
+	{
+		_putchar(str[counter]);
+		counter++;
+	}
+	_putchar('\n');
+}
