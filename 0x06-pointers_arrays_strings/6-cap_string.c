3d297f9aa231b91025c2afaf82d1d74d331a0d73
rotex <dantejake48@gmail.com>
2022-07-14 21:44:50 +0100

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+
+
+char *cap_string(char *str)
+{
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(str + count) >= 97 && *(str + count) <= 122)
+	{
+		*(str + count) = *(str + count) - 32;
+	}
+	count++;
+
+	while (*(str + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(str + count) == separators[i])
+			{
+
+				if ((*(str + (count + 1)) >= 97) && (*(str + (count + 1)) <= 122))
+					*(str + (count + 1)) = *(str + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
+	}
+	return (str);
+}
