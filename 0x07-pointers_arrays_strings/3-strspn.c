d1ef03a3696b2fbf2bff12ca799adc21b310516f
rotex <dantejake48@gmail.com>
2022-07-19 11:23:39 +0100

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to string to be scanned
+ * @accept: string that list of characters to match in s
+ * Return: Return length of a prefix substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int num;
+	int counter, i;
+
+	num = 0;
+	counter = 0;
+	while (*(s + counter))
+	{
+
+		/**num = 0;*/
+		for (i = 0; *(accept + i) != '\0'; i++)
+		{
+			if (*(accept + i) == *(s + counter))
+			{
+				num++;
+				break;
+			}
+		}
+		if (*(s + counter) != *(accept + i))
+		{
+			break;
+		}
+
+		counter++;
+	}
+	return (num);
+}
