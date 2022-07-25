e2a2b6bf23d899c07333a73ace409fb72304f0b5
rotex <dantejake48@gmail.com>
2022-07-25 21:31:41 +0100

    task 1 betty correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,15 +3,15 @@
 
 /**
  * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter. 
- *  @s: pointer to a string
+ * which contains a copy of the string given as a parameter.
+ *  @str: pointer to a string
  * Return: pointer to newly created array
  */
 
 char *_strdup(char *str)
 {
 	char *new_str;
-	int i,j;
+	int i, j;
 
 	if (str == NULL)
 	{
@@ -19,19 +19,19 @@ char *_strdup(char *str)
 	}
 
 	i = 0;
-	while(*(str + i) != '\0')
+	while (*(str + i) != '\0')
 	{
 		i++;
 	}
 
-	new_str = (char *)malloc((i+1) * sizeof(char));
+	new_str = (char *)malloc((i + 1) * sizeof(char));
 
 	if (new_str != NULL)
 	{
-		for (j=0; j<=i; j++)
+		for (j = 0; j <= i; j++)
 		{
 			*(new_str + j) = *(str + j);
 		}
 	}
-	return(new_str);
+	return (new_str);
 }
