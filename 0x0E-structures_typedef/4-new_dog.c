02830e7a49d56bd9d9f0ba808b3fe23b4c63eed5
rotex <dantejake48@gmail.com>
2022-08-01 23:21:42 +0100

    task 4 code refactor/bug fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * new_dog - creates a new dog
@@ -13,21 +14,41 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
-	char *new_name = name;
-	char *new_owner = owner;
+	dog_t *new_dog = NULL;
+	char *new_name = NULL, *new_owner = NULL;
+	int name_len = 0, owner_len = 0, i = 0, j = 0;
 
-	if (name == NULL || owner == NULL)
+	for (; *(name + name_len) != '\0'; i++)
+		name_len++;
+
+	for (; *(owner + owner_len) != '\0'; j++)
+		owner_len++;
+
+	new_dog = malloc(sizeof(*new_dog));
+	new_name = malloc(sizeof(*new_name) * (name_len + 1));
+	new_owner = malloc(sizeof(*new_owner) * (owner_len + 1));
+
+	if (new_dog == NULL || new_name == NULL || new_owner == NULL)
+	{
+		free(new_dog);
+		free(new_name);
+		free(new_owner);
 		return (NULL);
-
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
-		return (NULL);
-
-	ptr->name = new_name;
-	(*ptr).age = age;
-	ptr->owner = new_owner;
+	}
 
 
-	return (ptr);
+	for (; name_len > 0; name_len--)
+		*(new_name + name_len) = *(name + name_len);
+	*(new_name + name_len) = '\0';
+
+	for (; owner_len > 0; owner_len--)
+		*(new_owner + owner_len) = *(owner + owner_len);
+	*(new_owner + owner_len) = '\0';
+
+	new_dog->age = age;
+	(*new_dog).name = new_name;
+	new_dog->owner = new_owner;
+
+	return (new_dog);
 }
+
