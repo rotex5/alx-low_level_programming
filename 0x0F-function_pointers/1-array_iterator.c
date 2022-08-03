fd95cb5726131996f7f76f177fdfd7ff4789314c
rotex <dantejake48@gmail.com>
2022-08-03 21:21:11 +0100

    task 1 code refactor

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (array == NULL && action == NULL)
 		return;
 
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 		action(*(array + i));
 }
