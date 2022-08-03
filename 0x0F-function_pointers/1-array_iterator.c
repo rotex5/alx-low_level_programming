aab526e7ec356362a6c3719bed9cd57a65578596
rotex <dantejake48@gmail.com>
2022-08-03 21:26:45 +0100

    checker ohh..abeg work... na beg i dey beg

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
-	if (array == NULL && action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
-		action(array[i]);
+		action(*(array + i));
 }
