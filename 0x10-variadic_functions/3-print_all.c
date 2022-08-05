61842fbdff28c627d60e25f0f9a2f54d149f8aad
rotex <dantejake48@gmail.com>
2022-08-05 06:33:14 +0100

    task 3 code change(implemented using structs)

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,52 +3,87 @@
 #include <stdio.h>
 
 /**
+ * _print_char - print char
+ * @list : list of arguments passed to function
+ */
+void _print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * _print_int - print integers
+ * @list : list of arguments passed to function
+ */
+void _print_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * _print_float - print float
+ * @list : list of arguments passed to function
+ */
+void _print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * _print_str - print string
+ * @list : list of arguments passed to function
+ */
+void _print_str(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+
+/**
  * print_all - print anything
  * @format: string of formats used to identify what to be printed
  * Return: void
  */
 
-
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	va_list list;
+	int i = 0, j;
+	char *sep = "";
 
-	va_start(valist, format);
-	while (format && format[i])
+	user_select con[] = {
+		{"c", _print_char},
+		{"i", _print_int},
+		{"f", _print_float},
+		{"s", _print_str},
+		{NULL, NULL}
+	};
+
+	va_start(list, format);
+
+	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-		while (t_arg[j])
+		for (j = 0; con[j].c; j++)
 		{
-			if (format[i] == t_arg[j] && c)
+			if (format[i] == *con[j].c)
 			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
-				break;
+				printf("%s", sep);
+				con[j].f(list);
+				sep = ", ";
 			}
-			printf("%s", str);
-			break;
-		} i++;
+		}
+
+		i++;
 	}
-	printf("\n"), va_end(valist);
+
+	printf("\n");
+	va_end(list);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct selected - struct of character and it's corresponing function
+ * to be executed according.
+ * @c : the character passed
+ * @f : pointed to function to be executed based on 'c' entry
+ */
+
+typedef struct selected
+{
+	char *c;
+	void (*f)(va_list list);
+} user_select;
+
 #endif
