e72bea26b7cc8ffd79d2338a1277f583f273c922
rotex <dantejake48@gmail.com>
2022-08-05 05:28:18 +0100

    task 3 try out

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,41 +11,44 @@
 
 void print_all(const char * const format, ...)
 {
-	char *s = NULL, *empty = "(nil)", c = '\0';
-	int i = 0, _i = 0;
-	va_list ap;
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-	va_start(ap, format);
-	while (format != NULL && format[i] != '\0')
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		c = format[i++];
-		if (c == 'c' || c == 'i' || c == 'f' || c == 's')
+		j = 0;
+		while (t_arg[j])
 		{
-			_i = i;
-			while (_i++ == i && i > 1)
-				printf(", ");
-			switch (c)
+			if (format[i] == t_arg[j] && c)
 			{
-				case 'c':
-					printf("%c", va_arg(ap, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(ap, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(ap, double));
-					break;
-				case 's':
-					s = va_arg(ap, char *);
-					if (s == NULL)
-						s = empty;
-					printf("%s", s);
-					break;
-				default:
-					break;
-			}
+				printf(", ");
+				break;
+			} j++;
 		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
 	}
-	va_end(ap);
-	printf("\n");
+	printf("\n"), va_end(valist);
 }
