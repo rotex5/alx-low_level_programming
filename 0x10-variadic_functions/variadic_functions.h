656c9fa519de05a495765d0c2eb313262800707b
rotex <dantejake48@gmail.com>
2022-08-05 06:40:37 +0100

    header file code addition

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _print_char(va_list list);
+void _print_int(va_list list);
+void _print_float(va_list list);
+void _print_str(va_list list);
 
 /**
  * struct selected - struct of character and it's corresponing function
@@ -14,7 +18,6 @@ void print_all(const char * const format, ...);
  * @c : the character passed
  * @f : pointed to function to be executed based on 'c' entry
  */
-
 typedef struct selected
 {
 	char *c;
