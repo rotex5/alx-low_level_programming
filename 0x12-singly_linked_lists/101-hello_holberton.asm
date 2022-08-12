45849048fc02249701560e1efecb7279b0112e0c
rotex <dantejake48@gmail.com>
2022-08-12 05:34:59 +0100

    tasks 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+; printing "Hello, Holberton"
+
+;Author: rotex
+;Date: 12-Aug-2022
+
+section .text
+	global main
+main:
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4		;system call (sys_write)
+	int 0x80		;to call kernel
+
+	mov eax, 1		;system call (sys_exit)
+	int 0x080
+
+section .data
+	msg db "Hello, Holberton",0xA  ;0xA or 10 equates to newline
+	len equ $ -msg		   ;passing len of msg
