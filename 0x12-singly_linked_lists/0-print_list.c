4e684bbb5c59e272f903c1a5572f404005ff90e9
rotex <dantejake48@gmail.com>
2022-08-12 02:46:25 +0100

    tasks 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all the elements
+ * @h: head of list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
