6c26593249e528cd524670be78a31042b9d9a8e2
rotex <dantejake48@gmail.com>
2022-08-16 10:03:11 +0100

    task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - inerts a new node at a given position
+ * @head: head of the list
+ * @idx: index at which to insert
+ * @n: data (n) of the new node
+ *
+ * Return: The address of the new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node = NULL;
+	listint_t *cursor = *head;
+	listint_t *upper_next = NULL;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		while (cursor->next)
+		{
+
+			if ((count + 1) == idx)
+			{
+				upper_next = cursor->next;
+				cursor->next = new_node;
+				new_node->next = upper_next;
+				return (new_node);
+			}
+
+			count++;
+			cursor = cursor->next;
+		}
+	}
+	return (cursor);
+}
