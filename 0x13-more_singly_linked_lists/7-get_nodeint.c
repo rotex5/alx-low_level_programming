01a827e4e71fdbd595b260aa136111f92fd450f4
rotex <dantejake48@gmail.com>
2022-08-16 03:44:16 +0100

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t
+ * @head: head of the list
+ * @index: index of the node
+ * Return: the node at index if exits, or NULL otherwise
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *cursor = NULL;
+	unsigned int count = 0;
+
+	cursor = head;
+
+	if (cursor == NULL)
+		return (NULL);
+
+	while (cursor)
+	{
+		cursor =  cursor->next;
+		count++;
+		if (count == index)
+		{
+			break;
+		}
+
+	}
+	return (cursor);
+}
