02e9f9f8c5d84948fe8155430d4d981124239317
rotex <dantejake48@gmail.com>
2022-08-16 11:39:46 +0100

    task 10 first condition code fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *upper_next = NULL;
 	unsigned int count = 0;
 
-	if (!head)
+	if (!*head)
 		return (-1);
 
 	if (index == 0)
