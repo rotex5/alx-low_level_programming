3938013a47736b1d49cbb56be5c8d1c3a49d2e62
rotex <dantejake48@gmail.com>
2022-08-16 10:28:07 +0100

    check for size of list against index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,9 +13,19 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = NULL;
+	listint_t *checker = *head;
 	listint_t *cursor = *head;
 	listint_t *upper_next = NULL;
-	unsigned int count = 0;
+	unsigned int count = 0, size = 0;
+
+	while (checker)
+	{
+		size++;
+		checker = checker->next;
+	}
+
+	if (idx > size)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 
