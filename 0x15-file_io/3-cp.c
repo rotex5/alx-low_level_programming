e590411c269d708fc667e88172b75d2941a73864
rotex <dantejake48@gmail.com>
2022-08-22 21:09:39 +0100

    tasks 3 code change, checker 7,11,12,16,17,20 fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,43 +15,44 @@
 
 int main(int ac, char **av)
 {
-	int s_fd = 0, d_fd = 0;
+	int src_fd = 0, dst_fd = 0;
 	char buffer[1024];
-	ssize_t tn_read = 0, tn_wrote = 0;
+	int tn_read = 0, tn_wrote = 0;
 	mode_t mode;
 
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	/* or 0664 */
 
 	if (ac != 3)
 		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
 
-	s_fd = open(av[1], O_RDONLY);
-	if (s_fd < 0)
+	src_fd = open(av[1], O_RDONLY);
+	if (src_fd < 0)
 		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	tn_read = read(s_fd, buffer, SIZE);
+	dst_fd  = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (dst_fd < 0)
+		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+
+	do {
+		tn_read = read(src_fd, buffer, SIZE);
+		if (tn_read == -1)
+			dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+		if (tn_read > 0)
+		{
+			tn_wrote = write(dst_fd, buffer, tn_read);
+			if (tn_wrote < 0)
+				dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
+	} while (tn_read > 0);
+	tn_read = close(src_fd);
 	if (tn_read < 0)
-	{
-		close(s_fd);
-		dprintf(SE, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(SE, "Error: Can't write to %d\n", src_fd), exit(100);
 
-	d_fd  = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (d_fd < 0)
-	{
-		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
-	}
-
-
-	tn_wrote = write(d_fd, buffer, tn_read);
+	tn_wrote = close(dst_fd);
 	if (tn_wrote < 0)
-		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(SE, "Error: Can't write to %d\n", dst_fd), exit(100);
 
-	close(s_fd);
-	close(d_fd);
-
-	if (!close(s_fd) || !close(d_fd))
-		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(100);
 	return (0);
 }
