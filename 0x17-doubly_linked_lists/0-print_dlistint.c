a3be5908ff83dc34fcef95c417f5f7234488ba00
rotex <dantejake48@gmail.com>
2022-09-29 15:14:21 +0100

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dlistint - print all the elements
+ * @h: head of list
+ * Return: nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
