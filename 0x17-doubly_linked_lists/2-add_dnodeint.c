c27ef3ec9d716a7a709bff6cbd0e942c54820905
rotex <dantejake48@gmail.com>
2022-09-29 15:37:20 +0100

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - add node at beginning
+ * @head: points to pointer to head
+ * @n: integer to be inserted
+ * Return: address of the new element or NuLL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode = NULL;
+
+	if (!head)
+		return (NULL);
+
+	newNode = malloc(sizeof(dlistint_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = *head;
+	*head = newNode;
+
+	return (*head);
+}
