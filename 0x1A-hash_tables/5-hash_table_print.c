5ff6fc3374e8cd1ca137e123445f7eec52e03453
rotex <dantejake48@gmail.com>
2022-10-21 06:37:05 +0100

    task 5 fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t  *cursor = NULL;
-	unsigned long int i = 0, toggle = 1;
+	unsigned long int i = 0, toggle = 0;
 
 	if (!ht)
 		return;
@@ -25,12 +25,12 @@ void hash_table_print(const hash_table_t *ht)
 			if (toggle)
 				printf(", ");
 			printf("'%s': '%s'", cursor->key, cursor->value);
-			toggle = 0;
+			toggle = 1;
 
 			if (cursor->next != NULL)
 			{
 				printf(", ");
-				toggle = 1;
+				toggle = 0;
 			}
 			cursor = cursor->next;
 		}
